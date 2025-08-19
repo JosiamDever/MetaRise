ceaba795e7088727b28fc95458c4cb060b477487
JosiamDever <sjosian8@gmail.com>
2025-08-19 15:52:05 -0300

    SidebarEstilizada

--- main.js
+++ main.js
@@ -4,11 +4,13 @@ btnInit.addEventListener('click',() => {
   
   const sideBar = document.querySelector('.conteiner-sidebar');
   
+  const exitBtn = document.getElementById('navbar-exit');
+  
   sideBar.classList.contains('sideBarAtiva') 
    
    //se conter,remove
    ? sideBar.classList.remove('sideBarAtiva')
-   
+     
    //se não,add 
    : sideBar.classList.add('sideBarAtiva')
   
