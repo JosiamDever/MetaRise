c2f6505e4056673dfc5a8c41aea65b35fc879f08
JosiamDever <sjosian8@gmail.com>
2025-08-19 11:05:51 -0300

    Rolagem Fluída da SideBar configurada com operadores ternários

---
+++ main.js
@@ -0,0 +1,15 @@
+let btnInit = document.getElementById('navbar-bars');
+
+btnInit.addEventListener('click',() => {
+  
+  const sideBar = document.querySelector('.conteiner-sidebar');
+  
+  sideBar.classList.contains('sideBarAtiva') 
+   
+   //se conter,remove
+   ? sideBar.classList.remove('sideBarAtiva')
+   
+   //se não,add 
+   : sideBar.classList.add('sideBarAtiva')
+  
+});
